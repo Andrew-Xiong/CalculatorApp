5f970bc4532d319da7b5fc20e364e0e86f66a9d6
Jinzoh <andrewxiong@csus.edu>
2026-01-28 19:13:00 -0800

    Created actual window for calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,5 +8,15 @@ import javax.swing.border.LineBorder;
 public class Calculator {
     int boardWidth = 360;
     int boardHeight = 560;
-    
+
+    JFrame frame = new JFrame("Calculator");
+
+    Calculator(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
