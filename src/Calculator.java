b9652db0c7498db7e0cb54c124472623c4efff8d
Jinzoh <andrewxiong@csus.edu>
2026-02-04 18:58:01 -0800

    added jpanel and jlabel

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,7 +9,13 @@ public class Calculator {
     int boardWidth = 360;
     int boardHeight = 560;
 
+    Color customLightGrey = new Color(212, 212, 210);
+    Color customDarkGrey = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+
     JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
 
     Calculator(){
         frame.setVisible(true);
@@ -18,5 +24,8 @@ public class Calculator {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(customBlack);
+        
     }
 }
